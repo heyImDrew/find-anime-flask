c2aca4729a589b04377da619e4f60a61abb3a321
Andrey Kulakov <andreykulakov@getpinwheel.com>
2021-07-14 19:45:11 +0300

    Beautify code

--- app.py
+++ app.py
@@ -10,13 +10,12 @@ def index():
 
 @app.route('/search', methods=["GET", "POST"])
 def search():
-    search_name = "NULL"
-    results = []
     if request.method == "POST":
         search_name = request.form['name']
         api_response = requests.get(f"https://api.jikan.moe/v3/search/anime?q={search_name}").json()
         results = api_response.get('results', [])
-    return flask.render_template('search.html', search_name=search_name, results=results)
+        return flask.render_template('search.html', search_name=search_name, results=results)
+    return flask.render_template('search.html', search_name="", results=[])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=7007)
